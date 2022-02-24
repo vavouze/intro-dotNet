a399c8129a9172b15c5f0e5980261af6fab3dcd0
Louis Morando <louis.morando@epita.fr>
2022-02-24 12:32:16 +0100

    delete person

---
+++ Presentation/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DataProvider;
+
+namespace Presentation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly PersonRepository _personRepository = new PersonRepository(
+            );
+
+        private readonly ObservableCollection<Person> _persons;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _persons = new ObservableCollection<Person>(_personRepository.GetAll());
+            dgUsers.ItemsSource = _persons;
+        }
+
+        private void DeletePerson(object sender, RoutedEventArgs e)
+        {
+            var selectedPerson = (Person) dgUsers.SelectedItem;
+            if (_personRepository.Delete(selectedPerson.PersonId))
+            {
+                _persons.Remove(selectedPerson);
+            }
+        }
+    }
+}
